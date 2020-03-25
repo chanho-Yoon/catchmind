279c1ee3703058990ea66eb1135c1f3c74ad40b8
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-25 19:21:41 +0900

    import socketIO , morgan 및 socket 테스트

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 import { join } from 'path'
 import express from 'express'
+import socketIO from 'socket.io'
+import morgan from 'morgan'
+
 const PORT = 4000
 const app = express()
 
 app.set('view engine', 'pug')
+app.use(morgan('dev'))
 app.set('views', join(__dirname, 'views'))
+app.use(express.static(join(__dirname, 'static')))
+
+app.get('/', (req, res) => res.render('home'))
 
 const handleListening = () => console.log(`✅ Server running: http://localhost:${PORT}`)
 
-app.listen(PORT, handleListening)
+const server = app.listen(PORT, handleListening)
 
-app.get('/', (req, res) => res.render('home'))
+const io = socketIO.listen(server)
+
+let sockets = []
+io.on('connection', socket => {
+  sockets.push(socket.id)
+})
