33b549760b8f1ee4d014d61a111f3619b58fc8d8
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-30 23:43:47 +0900

    socketController.js 작성 및 server.js에서 import 시킴

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import { join } from 'path';
 import express from 'express';
 import socketIO from 'socket.io';
 import morgan from 'morgan';
+import socketController from './socketController';
 
 const PORT = 4000;
 const app = express();
@@ -14,20 +15,9 @@ app.use(express.static(join(__dirname, 'static')));
 app.get('/', (req, res) => res.render('home'));
 
 const handleListening = () => console.log(`✅ Server running: http://localhost:${PORT}`);
-
 const server = app.listen(PORT, handleListening);
-
+// socket io
 const io = socketIO.listen(server);
 
 let sockets = [];
-io.on('connection', socket => {
-  socket.on('newMessage', ({ message }) => {
-    socket.broadcast.emit('messageNotif', {
-      message,
-      nickname: socket.nickname || 'null'
-    });
-  });
-  socket.on('setNickname', ({ nickname }) => {
-    socket.nickname = nickname;
-  });
-});
+io.on('connection', socket => socketController);
