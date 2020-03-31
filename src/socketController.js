6480a6bef91827c208c9aaefe3c64d76d72b6d06
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-03-31 18:57:46 +0900

    로그인, 로그아웃 시 socket.on event 작성

--- src/socketController.js
+++ src/socketController.js
@@ -1,9 +1,14 @@
 import events from './events';
 
 const socketController = socket => {
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
+
   socket.on(events.setNickname, ({ nickname }) => {
-    console.log(nickname);
+    broadcast(events.newUser, { nickname });
     socket.nickname = nickname;
+  });
+  socket.on(events.disconnect, ({ nickname }) => {
+    broadcast(events.byeUser, { nickname: socket.nickname });
   });
 };
 
