2473e2635cc4c0de80b47cec706510b3dc2cc6cb
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-01 17:35:51 +0900

    NewUser와 ByeUser의 알림별 배경색

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,17 +1,17 @@
 const notifications = document.getElementById('jsNotifications');
 
-const fireNotification = (text, color, textcolor) => {
+const fireNotification = (text, color) => {
   const notification = document.createElement('div');
   notification.innerText = text;
   notification.style.backgroundColor = color;
-  notification.style.color = textcolor;
+  notification.className = 'notification';
   notifications.appendChild(notification);
 };
 
 export const handleNewUser = ({ nickname }) => {
-  fireNotification(`${nickname} 님 입장!`, 'rgb(0,122,255)', 'rgb(255,255,255)');
+  fireNotification(`${nickname} 님 입장!`, 'rgb(0, 122, 255)');
 };
 
 export const handleByeUser = ({ nickname }) => {
-  fireNotification(`${nickname} 님 퇴장!`, 'rgb(255, 149, 0)', 'rgb(255,255,255)');
+  fireNotification(`${nickname} 님 퇴장!`, 'rgb(255, 149, 0)');
 };
