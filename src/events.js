b19ec4e09cf8587777291d3c38117e71fb8a2748
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-01 22:10:28 +0900

    socket event add => disconnet, setMessage, receiveMessage

--- src/events.js
+++ src/events.js
@@ -2,7 +2,9 @@ const events = {
   setNickname: 'setNickname',
   newUser: 'newUser',
   byeUser: 'byeUser',
-  disconnect: 'disconnect'
+  disconnect: 'disconnect',
+  setMessage: 'setMessage',
+  receiveMessage: 'receiveMessage'
 };
 
 export default events;
