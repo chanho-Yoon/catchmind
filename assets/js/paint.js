0d9f2187ad9188dbaf8a537ba1fa88b77a39b115
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-03 16:23:40 +0900

    beginPath(시작좌표), strokedPath(그리기) 함수를 따로빼서 socket에서 handler로 사용할 수 있게함

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -13,21 +13,29 @@ canvas.height = 600;
 
 //canvas 배경색을 처음에 초기화시켜주지 않아서 아무 색상없이 이미지 저장시 바탕화면이 투명하게 저장됨 이것을 초기화로 해결
 ctx.fillStyle = 'white';
+ctx.strokeStyle = 'black';
 ctx.fillRect(0, 0, 700, 600);
 ctx.lineWidth = 2.5;
 
 let painting = false;
+
+const beginPath = (x, y) => {
+  ctx.beginPath(); //새로운 선을 그리겠다는 선언
+  ctx.moveTo(x, y); //시작 좌표로 시작점
+};
+const strokedPath = (x, y) => {
+  ctx.lineTo(x, y); //끝나는 좌표로 어디까지 그리는지 나타냄
+  ctx.stroke(); // stroke()메서드를 사용해야 canvas위에 선이 그려짐
+};
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (painting === false) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    console.log(`false : ${painting}`);
+    beginPath(x, y); //마우스가 업 상태일때 시작지점을 알기위해 경로를 알아야함
+    getSocket().emit(window.events.beginPath, { x, y });
   } else {
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    console.log(`true : ${painting}`);
+    strokedPath(x, y); //마
+    getSocket().emit(window.events.strokePath, { x, y });
   }
 }
 function stopPainting() {
@@ -66,3 +74,6 @@ function handleClickJsmode() {
 if (jsmode) {
   jsmode.addEventListener('click', handleClickJsmode);
 }
+
+export const handleBeganPath = ({ x, y }) => beginPath(x, y);
+export const handleStrokedPath = ({ x, y }) => strokedPath(x, y);
