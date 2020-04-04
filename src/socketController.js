ef97003f3219ce02211615d92633554668bbaffd
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-04 17:01:41 +0900

    접속한 유저 확인(추적) 및 접속을 끊은 유저 확인

--- src/socketController.js
+++ src/socketController.js
@@ -1,14 +1,23 @@
 import events from './events';
+// 참여중인 유저 추적
+let sockets = [];
 
-const socketController = socket => {
+const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
+  const allBroadcast = (event, data) => io.emit(event, data);
+  const sendPlayerUpdate = () => allBroadcast(events.playerUpdate, { sockets });
 
   socket.on(events.setNickname, ({ nickname }) => {
-    broadcast(events.newUser, { nickname });
     socket.nickname = nickname;
+    sockets.push({ id: socket.id, points: 0, nickname: nickname });
+    broadcast(events.newUser, { nickname });
+    sendPlayerUpdate();
   });
   socket.on(events.disconnect, ({ nickname }) => {
+    // disconnect 한 socket의 nickname을 가지고 있지 않은 socket만 찾음
+    sockets = sockets.filter((aSocket) => aSocket.id !== socket.id);
     broadcast(events.byeUser, { nickname: socket.nickname });
+    sendPlayerUpdate();
   });
   // 유저가 메시지 입력하면 자신 이외 사용자에게
   socket.on(events.setMessage, ({ message }) => {
