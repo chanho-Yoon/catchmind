5b88d7aeb907269a50288f3a7c9765d726dfa401
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-04 17:02:56 +0900

    connect, disconnect player 정보 update 이벤트 추가

--- src/events.js
+++ src/events.js
@@ -12,7 +12,8 @@ const events = {
   getColor: 'getColor',
   setColor: 'setColor',
   getFillColor: 'getFillColor', // socketController에서 emit한 color 가져옴
-  setFillColor: 'setFillColor' // socketController에서 broadcast하여 다른 유저에게 sockets.js로 보냄
+  setFillColor: 'setFillColor', // socketController에서 broadcast하여 다른 유저에게 sockets.js로 보냄
+  playerUpdate: 'playterUpdate',
 };
 
 export default events;
