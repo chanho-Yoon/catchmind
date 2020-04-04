b0cfe6630a4c2b2dfc23eec18d9e7f315679f516
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-04 19:32:40 +0900

    플레이어 참여자 view에 출력하는 js 작성

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,1 +1,12 @@
-export const handlePlayerUpdate = ({ sockets }) => console.log(sockets);
+const board = document.getElementById('jsPlayerBoard');
+
+const addPlayers = (players) => {
+  board.innerHTML = '';
+  players.forEach((player) => {
+    const playerElemnet = document.createElement('span');
+    playerElemnet.innerText = `${player.nickname}: ${player.points}`;
+    board.appendChild(playerElemnet);
+  });
+};
+
+export const handlePlayerUpdate = ({ sockets }) => addPlayers(sockets);
