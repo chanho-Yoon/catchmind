b4e40b54dd0759de5f347f2b50bb45127087ab9a
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-04 17:03:16 +0900

    이름 변경 aSocket -> socket

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,22 +1,22 @@
 import { handleNewUser, handleByeUser } from './notifications';
 import { handleReceiveMessage } from './chat';
 import { handleBeganPath, handleStrokedPath, handleSetColor, handleSetFillColor } from './paint';
+import { handlePlayerUpdate } from './players';
 
 let socket = null;
 
 export const getSocket = () => socket;
-export const updateSocket = aSocket => {
-  socket = aSocket;
-};
+
 // 로그인 한 시점에 socket을 시작하도록
-export const initSockets = aSocket => {
+export const initSockets = (aSocket) => {
   const { events } = window;
-  updateSocket(aSocket);
-  aSocket.on(events.newUser, handleNewUser);
-  aSocket.on(events.byeUser, handleByeUser);
-  aSocket.on(events.receiveMessage, handleReceiveMessage); //본인 이외 다른 유저에게 보여질 메시지 소켓
-  aSocket.on(events.beganPath, handleBeganPath); // 마우스 업상태에서 시작좌표 지점
-  aSocket.on(events.strokedPath, handleStrokedPath); // 마우스 다운상태에서 라인 그리기
-  aSocket.on(events.setColor, handleSetColor); // 컬러 공유
-  aSocket.on(events.setFillColor, handleSetFillColor);
+  socket = aSocket;
+  socket.on(events.newUser, handleNewUser);
+  socket.on(events.byeUser, handleByeUser);
+  socket.on(events.receiveMessage, handleReceiveMessage); //본인 이외 다른 유저에게 보여질 메시지 소켓
+  socket.on(events.beganPath, handleBeganPath); // 마우스 업상태에서 시작좌표 지점
+  socket.on(events.strokedPath, handleStrokedPath); // 마우스 다운상태에서 라인 그리기
+  socket.on(events.setColor, handleSetColor); // 라인 컬러
+  socket.on(events.setFillColor, handleSetFillColor); // 배경 컬러
+  socket.on(events.playerUpdate, handlePlayerUpdate);
 };
