4b42d0c4e8810648348185251a02970433f53d64
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-04 19:31:51 +0900

    게임 시작하는 함수 startGame 작성 및 방장,단어 랜덤선택 함수 작성

--- src/socketController.js
+++ src/socketController.js
@@ -1,17 +1,32 @@
 import events from './events';
+import { chooseWord } from './words';
 // 참여중인 유저 추적
 let sockets = [];
+let inProgress = false; //false -> 게임 시작
+let word = null;
+
+//게임 리더 정하는 랜덤함수
+const chooseLeader = () => sockets[Math.floor(Math.random() * sockets.length)];
 
 const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
   const allBroadcast = (event, data) => io.emit(event, data);
   const sendPlayerUpdate = () => allBroadcast(events.playerUpdate, { sockets });
+  // 게임 시작
+  const startGame = () => {
+    if (inProgress === false) {
+      inProgress = true;
+      const painter = chooseLeader(); //리더 랜덤 선택
+      word = chooseWord(); // 단어 랜덤 선택
+    }
+  };
 
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
     sockets.push({ id: socket.id, points: 0, nickname: nickname });
     broadcast(events.newUser, { nickname });
     sendPlayerUpdate();
+    startGame();
   });
   socket.on(events.disconnect, ({ nickname }) => {
     // disconnect 한 socket의 nickname을 가지고 있지 않은 socket만 찾음
