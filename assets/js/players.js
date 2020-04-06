2056ffb90f273ed243c593beed369ab4f36051aa
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-06 18:58:15 +0900

    출제자 이외 canvas 이벤트 삭제 및 게임 종료시 알림창

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,4 +1,7 @@
+import { disableCanvas, hideControls, enableCanvas, showControls, showWord, hideWord, resetCanvas } from './paint';
+
 const board = document.getElementById('jsPlayerBoard');
+const notifs = document.getElementById('jsWords');
 
 const addPlayers = (players) => {
   board.innerHTML = '';
@@ -9,4 +12,28 @@ const addPlayers = (players) => {
   });
 };
 
+const setNotifs = (text) => {
+  notifs.innerHTML = '';
+  notifs.innerHTML = text;
+};
+
 export const handlePlayerUpdate = ({ sockets }) => addPlayers(sockets);
+//방장 이외에는 canvas 이벤트 동작을 막음
+export const handleGameStart = () => {
+  setNotifs();
+  disableCanvas();
+  hideControls();
+  hideWord();
+};
+export const handleLeaderNotification = ({ word }) => {
+  enableCanvas();
+  showControls();
+  showWord();
+  setNotifs(`<span>${word}</span>`);
+};
+
+export const handleGameEnded = () => {
+  setNotifs('게임 끝👍🏻');
+  showWord();
+  resetCanvas();
+};
