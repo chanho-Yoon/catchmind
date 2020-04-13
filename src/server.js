38ae92803b312a6f09ed0f9a84492c382276a6f6
chanho-Yoon <chanho.yoon.1993@gmail.com>
2020-04-14 02:11:37 +0900

    경로 수정 및 import dotenv

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ import socketIO from 'socket.io';
 import morgan from 'morgan';
 import socketController from './socketController';
 import events from './events';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = 4000;
 const app = express();
@@ -16,8 +19,8 @@ app.use(express.static(join(__dirname, 'static')));
 app.get('/', (req, res) => res.render('home', { events: JSON.stringify(events) }));
 
 const handleListening = () => console.log(`✅ Server running: http://localhost:${PORT}`);
-const server = app.listen(PORT, handleListening);
+const server = app.listen(process.env.PORT || 3000, handleListening);
 // socket io
 const io = socketIO.listen(server);
 
-io.on('connection', (socket) => socketController(socket, io));
+io.on('connection', socket => socketController(socket, io));
